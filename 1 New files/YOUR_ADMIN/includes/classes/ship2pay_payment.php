4a4054de51bbbc7ef77b275828c52889ed3ba1ed
Zen4All <info@zen4all.nl>
2016-07-27 22:44:55 +0200

    php 7 compatibilaty

--- 1 New files/YOUR_ADMIN/includes/classes/ship2pay_payment.php
+++ 1 New files/YOUR_ADMIN/includes/classes/ship2pay_payment.php
@@ -1,51 +1,53 @@
 <?php
+
 /*
-  $Id: Ship2Pay, v1.5 17:20 2008-06-27 gjw Exp $
-  modified by wladimirec
-*/
+ * $Id: Ship2Pay, v1.8 11:05 2016-05-11 Zen4All $
+ */
 
-  class payment {
-    var $modules;
-    
-// class constructor
-    function payment() {
+class payment {
 
-      if (defined('MODULE_PAYMENT_INSTALLED') && zen_not_null(MODULE_PAYMENT_INSTALLED)) {
-        $allmods = explode(';', MODULE_PAYMENT_INSTALLED);
-        
-        $this->modules = array();
-        
-        for ($i = 0, $n = sizeof($allmods); $i < $n; $i++) {
-          $file = $allmods[$i];
-          $class = substr($file, 0, strrpos($file, '.'));
-          $this->modules[$i]['class'] = $class;
-          $this->modules[$i]['file'] = $file;
-        }
+  var $modules;
+
+  function __construct() {
+
+    if (defined('MODULE_PAYMENT_INSTALLED') && zen_not_null(MODULE_PAYMENT_INSTALLED)) {
+      $allmods = explode(';', MODULE_PAYMENT_INSTALLED);
+
+      $this->modules = array();
+
+      for ($i = 0, $n = sizeof($allmods); $i < $n; $i++) {
+        $file = $allmods[$i];
+        $class = substr($file, 0, strrpos($file, '.'));
+        $this->modules[$i]['class'] = $class;
+        $this->modules[$i]['file'] = $file;
       }
-    }
-    
-    function get_modules(){
-      return $this->modules;
-    }
-    
-    function payment_multiselect($parameters, $selected = '') {
-      $arrSelected = explode(';', $selected);
-      $select_string = '<select ' . $parameters . ' multiple size="10">';
-      for ($i = 0, $n = sizeof($this->modules); $i < $n; $i++) {
-        $sFile = $this->modules[$i]['file'];
-        $sClass = $this->modules[$i]['class'];
-        $select_string .= '<option value="' . $sFile . '"';
-        if (in_array($sFile,$arrSelected)) $select_string .= ' SELECTED';
-        $select_string .= '>' . $sClass . '</option>';
-      }
-      $select_string .= '</select>';
-      return $select_string;
-    }
-    
-    function GetModuleName($sSelected){
-      $sNames = str_replace(".php", "", $sSelected);
-      $sNames = str_replace(";", ",&nbsp;", $sNames);
-      return $sNames;
     }
   }
-?>
+
+  function get_modules() {
+    return $this->modules;
+  }
+
+  function payment_multiselect($parameters, $selected = '') {
+    $arrSelected = explode(';', $selected);
+    $select_string = '<select ' . $parameters . ' multiple size="10">';
+    for ($i = 0, $n = sizeof($this->modules); $i < $n; $i++) {
+      $sFile = $this->modules[$i]['file'];
+      $sClass = $this->modules[$i]['class'];
+      $select_string .= '<option value="' . $sFile . '"';
+      if (in_array($sFile, $arrSelected)) {
+        $select_string .= ' SELECTED';
+      }
+      $select_string .= '>' . $sClass . '</option>';
+    }
+    $select_string .= '</select>';
+    return $select_string;
+  }
+
+  function GetModuleName($sSelected) {
+    $sNames = str_replace(".php", "", $sSelected);
+    $sNames = str_replace(";", ",&nbsp;", $sNames);
+    return $sNames;
+  }
+
+}
--- 1 New files/includes/classes/ship2pay.php
+++ 1 New files/includes/classes/ship2pay.php
@@ -1,20 +1,16 @@
 <?php
 
 /*
-  $Id: Ship2Pay, v1.5 17:18 2008-06-27 gjw Exp $
-  modified by wladimirec
-
+ * $Id: Ship2Pay, v1.8 11:05 2016-05-11 Zen4All $
  */
 
-////
 // Class to handle links between shipping and payment
 
 class ship2pay {
 
   var $modules;
 
-// class constructor
-  function ship2pay() {
+  function __construct() {
     global $db;
     $this->modules = array();
     $mods = $db->Execute("SELECT shipment, payments_allowed FROM " . TABLE_SHIP2PAY . " WHERE status = 1");
