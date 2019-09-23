6b0c221ccef546e8235de46bebd99b98fd4d7cc2
Zen4All <info@zen4all.nl>
2019-09-23 09:45:34 +0200

    updates

--- 1 New files/YOUR_ADMIN/includes/classes/ship2pay_shipping.php
+++ 1 New files/YOUR_ADMIN/includes/classes/ship2pay_shipping.php
@@ -8,28 +8,30 @@ class shipping {
 
   var $modules;
 
-  function __construct() {
+  function __construct()
+  {
 
     if (defined('MODULE_SHIPPING_INSTALLED') && zen_not_null(MODULE_SHIPPING_INSTALLED)) {
       $allmods = explode(';', MODULE_SHIPPING_INSTALLED);
 
-      $this->modules = array();
+      $this->modules = [];
 
       for ($i = 0, $n = sizeof($allmods); $i < $n; $i++) {
         $file = $allmods[$i];
         $class = substr($file, 0, strrpos($file, '.'));
-        $this->modules[$i] = array();
         $this->modules[$i]['class'] = $class;
         $this->modules[$i]['file'] = $file;
       }
     }
   }
 
-  function get_modules() {
+  function get_modules()
+  {
     return $this->modules;
   }
 
-  function shipping_select($parameters, $selected = '') {
+  function shipping_select($parameters, $selected = '')
+  {
     echo $selected;
     $select_string = '<select ' . $parameters . '>';
     for ($i = 0, $n = sizeof($this->modules); $i < $n; $i++) {
