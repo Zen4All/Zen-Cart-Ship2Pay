3d97ac29e841a6e33a2704f654efde6c3dac38ab
Zen4All <info@zen4all.nl>
2019-10-29 09:10:22 +0100

    update to Zen Cart 1.5.6 core code

--- 2 Core file overrides/includes/classes/payment.php
+++ 2 Core file overrides/includes/classes/payment.php
@@ -3,10 +3,10 @@
  * Payment Class.
  *
  * @package classes
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: zcwilt  Sat Feb 6 10:07:47 2016 +0000 Modified in v1.5.5 $
+ * @version $Id: DrByte 2019 Jul 16 Modified in v1.5.6c $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
@@ -46,17 +46,17 @@ class payment extends base {
 
         $include_modules[] = array('class' => $module, 'file' => $module . '.php');
       } else {
-        reset($this->modules);
 
         // Free Payment Only shows
-        if (zen_get_configuration_key_value('MODULE_PAYMENT_FREECHARGER_STATUS') and ($_SESSION['cart']->show_total()==0 and (!isset($_SESSION['shipping']['cost']) || $_SESSION['shipping']['cost'] == 0))) {
+        $freecharger_enabled = (defined('MODULE_PAYMENT_FREECHARGER_STATUS') && MODULE_PAYMENT_FREECHARGER_STATUS == 'True');
+        if ($freecharger_enabled && $_SESSION['cart']->show_total() == 0 && (!isset($_SESSION['shipping']['cost']) || $_SESSION['shipping']['cost'] == 0)) {
           $this->selected_module = $module;
           if (file_exists(DIR_FS_CATALOG . DIR_WS_MODULES . '/payment/' . 'freecharger.php')) {
             $include_modules[] = array('class'=> 'freecharger', 'file' => 'freecharger.php');
           }
         } else {
           // All Other Payment Modules show
-          while (list(, $value) = each($this->modules)) {
+          foreach($this->modules as $value) {
             // double check that the module really exists before adding to the array
             if (file_exists(DIR_FS_CATALOG . DIR_WS_MODULES . '/payment/' . $value)) {
               $class = substr($value, 0, strrpos($value, '.'));
@@ -119,6 +119,7 @@ class payment extends base {
   section. This should be looked into again post 2.2.
   */
   function update_status() {
+    if (empty($this->selected_module)) return; 
     if (is_array($this->modules)) {
       if (is_object($GLOBALS[$this->selected_module])) {
         if (method_exists($GLOBALS[$this->selected_module], 'update_status')) {
@@ -131,7 +132,7 @@ class payment extends base {
   function javascript_validation() {
     $js = '';
     if (is_array($this->modules) && sizeof($this->selection()) > 0) {
-      $js = '<script type="text/javascript"><!-- ' . "\n" .
+      $js = '<script type="text/javascript">' . "\n" .
       'function check_form() {' . "\n" .
       '  var error = 0;' . "\n" .
       '  var error_message = "' . JS_ERROR . '";' . "\n" .
@@ -150,10 +151,9 @@ class payment extends base {
       '    }' . "\n" .
       '  }' . "\n\n";
 
-      reset($this->modules);
-      while (list(, $value) = each($this->modules)) {
+      foreach($this->modules as $value) {
         $class = substr($value, 0, strrpos($value, '.'));
-        if ($GLOBALS[$class]->enabled) {
+        if (isset($GLOBALS[$class]->enabled) && $GLOBALS[$class]->enabled == true) {
           $js .= $GLOBALS[$class]->javascript_validation();
         }
       }
@@ -166,13 +166,13 @@ class payment extends base {
        $js =  $js .'    alert(error_message);' . "\n";
        $js =  $js . '    return false;' . "\n";
        $js =  $js .'  } else {' . "\n";
-       $js =  $js .' var result = true '  . "\n";
+       $js =  $js .' var result = true; '  . "\n";
        if ($this->doesCollectsCardDataOnsite == true && PADSS_AJAX_CHECKOUT == '1') {
          $js .= '      result = !(doesCollectsCardDataOnsite(payment_value));' . "\n";
        }
        $js =  $js .' if (result == false) doCollectsCardDataOnsite();' . "\n";
        $js =  $js .'    return result;' . "\n";
-       $js =  $js .'  }' . "\n" . '}' . "\n" . '//--></script>' . "\n";
+       $js =  $js .'  }' . "\n" . '}' . "\n" . '</script>' . "\n";
     }
     return $js;
   }
@@ -180,10 +180,9 @@ class payment extends base {
   function selection() {
     $selection_array = array();
     if (is_array($this->modules)) {
-      reset($this->modules);
-      while (list(, $value) = each($this->modules)) {
+      foreach($this->modules as $value) {
         $class = substr($value, 0, strrpos($value, '.'));
-        if ($GLOBALS[$class]->enabled) {
+        if (isset($GLOBALS[$class]->enabled) && $GLOBALS[$class]->enabled == true) {
           $selection = $GLOBALS[$class]->selection();
           if (isset($GLOBALS[$class]->collectsCardDataOnsite) && $GLOBALS[$class]->collectsCardDataOnsite == true) {
             $selection['fields'][] = array('title' => '',
@@ -200,10 +199,9 @@ class payment extends base {
   function in_special_checkout() {
     $result = false;
     if (is_array($this->modules)) {
-      reset($this->modules);
-      while (list(, $value) = each($this->modules)) {
+      foreach($this->modules as $value) {
         $class = substr($value, 0, strrpos($value, '.'));
-        if ($GLOBALS[$class]->enabled && method_exists($GLOBALS[$class], 'in_special_checkout')) {
+        if (isset($GLOBALS[$class]) && is_object($GLOBALS[$class]) && $GLOBALS[$class]->enabled && method_exists($GLOBALS[$class], 'in_special_checkout')) {
           $module_result = $GLOBALS[$class]->in_special_checkout();
           if ($module_result === true) $result = true;
         }
@@ -214,6 +212,7 @@ class payment extends base {
 
   function pre_confirmation_check() {
     global $credit_covers, $payment_modules;
+    if (empty($this->selected_module)) return; 
     if (is_array($this->modules)) {
       if (is_object($GLOBALS[$this->selected_module]) && ($GLOBALS[$this->selected_module]->enabled) ) {
         if ($credit_covers) {
@@ -228,11 +227,13 @@ class payment extends base {
   }
 
   function confirmation() {
-    if (is_array($this->modules)) {
-      if (is_object($GLOBALS[$this->selected_module]) && ($GLOBALS[$this->selected_module]->enabled) ) {
-        return $GLOBALS[$this->selected_module]->confirmation();
-      }
-    }
+    $default = array('title' => '', 'fields' => array());
+    if (!is_array($this->modules)) return $default;
+    if (!is_object($GLOBALS[$this->selected_module])) return $default;
+    if (!$GLOBALS[$this->selected_module]->enabled) return $default;
+    $confirmation = $GLOBALS[$this->selected_module]->confirmation();
+    if (!is_array($confirmation)) return $default;
+    return array_merge($default, $confirmation);
   }
 
   function process_button_ajax() {
@@ -299,5 +300,14 @@ class payment extends base {
       }
     }
     return array(false, '');
+  }
+
+  function clear_payment()
+  {
+    if (!is_array($this->modules)) return;
+    if (!is_object($GLOBALS[$this->selected_module])) return;
+    if (!$GLOBALS[$this->selected_module]->enabled) return;
+    if (!method_exists($GLOBALS[$this->selected_module], 'clear_payment')) return;
+    $GLOBALS[$this->selected_module]->clear_payment();
   }
 }
