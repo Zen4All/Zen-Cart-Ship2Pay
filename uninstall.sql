9ede1cf6b5628298bbd35803882be3733a754cf0
Zen4All <info@zen4all.nl>
2019-11-05 12:55:33 +0100

    Added extra code

--- uninstall.sql
+++ uninstall.sql
@@ -1,3 +1,5 @@
-DROP TABLE ship2pay
+DROP TABLE ship2pay;
 
-DELETE FROM admin_pages WHERE admin_pages.page_key = 'ship2pay'
+DELETE FROM admin_pages WHERE admin_pages.page_key = 'ship2pay';
+DELETE FROM admin_pages WHERE admin_pages.page_key = 'configShip2pay';
+DELETE FROM configuration_group WHERE configuration_group.configuration_group_title = 'Ship2Pay';
